4ca75379f58e0ab07428e2aee9e4fcf937dbb41f
ᴛʜᴇ ᴀɴᴏɴʏᴍᴏᴜs <TheAnonymous2510@gmail.com>
2022-11-19 16:56:50 +0530

    Update help.py

--- StormBeatz/plugins/bot/help.py
+++ StormBeatz/plugins/bot/help.py
@@ -29,17 +29,10 @@ HELP_COMMAND = get_command("HELP_COMMAND")
 
 
 @app.on_message(
-    filters.command(HELP_COMMAND)
-    & filters.group
-    & ~filters.edited
-    & ~BANNED_USERS
+    filters.command(HELP_COMMAND) & filters.private & ~filters.edited & ~BANNED_USERS
 )
-@app.on_callback_query(
-    filters.regex("settings_back_helper") & ~BANNED_USERS
-)
-async def helper_private(
-    client: app, update: Union[types.Message, types.CallbackQuery]
-):
+@app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
+async def helper_private(client: app, update: Union[types.Message, types.CallbackQuery]):
     is_callback = isinstance(update, types.CallbackQuery)
     if is_callback:
         try:
